74c1ae810366df48af90a9c15864c8d7d9c329bd
matiaszo <tiaszuniga@gmail.com>
2024-10-07 11:45:43 -0300

    filtro?

--- src/app/axios-page/page.tsx
+++ src/app/axios-page/page.tsx
@@ -8,7 +8,7 @@ import { error } from "console";
 
 interface IData {
     albumId: string;
-    url: string;
+    thumbnailUrl: string;
     title: string,
 }
 
@@ -16,43 +16,44 @@ const axiosPage = () =>  {
 
     const [local, setLocal] = useState<IData[]>([]);
     const [message, setMessage] = useState<string>("");
+    const [photoId, setPhotoId] = useState<string>("");
     
 
     useEffect(() => {
-        api.get("photos").then((res) =>{
+        api.get(`photos/${photoId}`).then((res) =>{
             const data = res.data
             console.log(data)
-            setLocal(data)
+            setLocal(Array.isArray(data) ? data : [data])
         }).catch((error) =>{
             if(error.response.status === 404){
-                setMessage("Pagina encontrou nao doidao")
+                setMessage("Pagina encontrou nao")
             }
         })
 
         return () =>{
 
         }
-    },[])
+    },[photoId])
 
 
     return(
         <>
             <div className="flex flex-col mt-2">
             <p>something</p>
+            <input type="text" placeholder="Digite o id" className="text-center" value={photoId} onChange={(event) => setPhotoId(event.target.value)}/>
+            <div className="flex flex-wrap items-center justify-center mt-5">
                 {local.map((item, index) =>{
                     return(
-                        <div key={index}>
-                            <p>--------inicio--------</p>
-                            <img src={item.url} alt="imagem" width={200} height={200}/>
-                            <p>{item.url}</p>
+                        <div key={index} className="flex flex-col w-1/3 items-center justify-center">
+                            <img src={item.thumbnailUrl} alt="imagem" width={150} height={150}/>
+                            <p>{item.thumbnailUrl}</p>
                             <p>{item.title}</p>
                             <p>{item.albumId}</p>
-                            <p>--------final---------</p>
                             <br />
-
                         </div>
                     )
                 })}
+                </div>
             </div>
         </>
     )
